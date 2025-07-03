9afd052291aed8f93324d2c57d298da4173188cc
KalpShah1384 <kalpshah1384@gmail.com>
2025-07-03 15:09:39 +0530

    touchy

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ import adminrouter from './routes/adminRoute.js';
 import doctorRouter from './routes/doctorRoute.js';
 import userRouter from './routes/userRoute.js';
 import contactRouter from './routes/contact.js';
+import getInTouchRouter from './routes/getintouch.js';
 
 //app configuration
 const app = express();
@@ -24,6 +25,7 @@ app.use('/api/admin', adminrouter);
 app.use('/api/doctor', doctorRouter);
 app.use('/api/user', userRouter);
 app.use('/api/contact', contactRouter);
+app.use('/api/getintouch', getInTouchRouter);
 
 //localhost:4000/api/admin/add-doctor
 app.get('/', (req, res) => {
